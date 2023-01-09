c27628a5ef6f542c40d602c708108da57d5bad42
GiovannyCortes <giovannyandres.cortes@outlook.com>
2023-01-09 22:01:16 +0100

    Modify time

--- src/components/Tiempo.js
+++ src/components/Tiempo.js
@@ -13,10 +13,12 @@ function App() {
 
     useEffect(() => {
         socket.on('envio', (num) => {
-            if (num === 10) {
-                navigator.vibrate([1000,500,1000,500,1000]);
-            } else if (num === 3) {
-                navigator.vibrate([2500]);
+            if (num === 12) {
+                navigator.vibrate([1000,1000,1000,1000,1000]);
+            }
+            
+            if (num === 3) {
+                navigator.vibrate(2500);
             }
             const min = Math.floor(num / 60);
             const sec = num % 60;
