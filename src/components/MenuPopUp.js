878b510c946abb4dcba5e241e6e46b25fad4cac9
GiovannyCortes <giovannyandres.cortes@outlook.com>
2023-01-23 22:43:24 +0100

    Insert Authors

--- src/components/MenuPopUp.js
+++ src/components/MenuPopUp.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './css/MenuPopUp.css';
 
+import GitHubLogo from '../assets/github/icons8-github-48(-ldpi).png';
+import LinkedinLogo from '../assets/linkedin/icons8-linkedin-rodeado-de-círculo-48(-mdpi).png';
+
 /* 
     En la medida de lo posible, trabajaremos con SVG para no cargar la App 
     con peso innecesario de imagenes para formas o iconos.
@@ -27,6 +30,27 @@ export class MenuPopUp extends Component {
                     <NavLink to='/categorias' onClick={this.exit}>Categorías</NavLink>                     {/* CRUD de las categorías */}
                     <NavLink to='/temporizadores' onClick={this.exit}>Temporizadores</NavLink>             {/* CRUD de los temporizadores */}
                     <NavLink to='/login' onClick={this.exit}>Administrador</NavLink>                       {/* Login para obenter permisos CUD del CRUD */}
+                </div>
+                <p id='authors_title'>Aplicación desarrollada por: </p>
+                <div className='authors_box'>
+                    <div>
+                        <p className='authors'>Giovanny Cortés Hernández</p>
+                        <a href='https://github.com/giovannycortes' target="_blank" className='authors_btns' rel="noreferrer">
+                            <img src={GitHubLogo} alt='GitHub logo'/>
+                        </a>
+                        <a href='https://es.linkedin.com/in/giovannycortes' target="_blank" className='authors_btns' rel="noreferrer">
+                            <img src={LinkedinLogo} alt='Linkedin logo'/>
+                        </a>
+                    </div>
+                    <div>
+                        <p className='authors'>Álvaro Gutierrez Carnicero</p>
+                        <a href='https://github.com/BURNI80' target="_blank" className='authors_btns' rel="noreferrer">
+                            <img src={GitHubLogo} alt='GitHub logo'/>
+                        </a>
+                        <a href='https://es.linkedin.com/in/alvaro-gutierrez-carnicero' target="_blank" className='authors_btns' rel="noreferrer">
+                            <img src={LinkedinLogo} alt='Linkedin logo'/>
+                        </a>
+                    </div>
                 </div>
             </div>
         )
