7093db8577544158613eedb75446909ab7ec3cc1
Álvaro Gutiérrez Carnicero <77487911+BURNI80@users.noreply.github.com>
2023-01-31 13:01:55 +0100

    Cambio URL

--- src/Global.js
+++ src/Global.js
@@ -1,9 +1,9 @@
 var Global = {
     // mainUrl : "https://localhost:7004/", // Importante que tenga el slash final,
-    mainUrl : "https://apitimersagc.azurewebsites.net/", // Importante que tenga el slash final,
+    mainUrl : "https://apitimersbcn.azurewebsites.net/", // Importante que tenga el slash final,
     // mainUrl: "https://apitesttimers.azurewebsites.net/", // PACO
     // SocketUrl : "https://syncserver-production.up.railway.app/"
-    SocketUrl : "https://synctimerserverfinal.azurewebsites.net/"
+    SocketUrl : "https://syncserverbcn.azurewebsites.net/"
 }
 
 export default Global;
